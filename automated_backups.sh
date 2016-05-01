8c6d3fa00b3a3be560d423ba9cf62cfce3bc3964
Robert <gnu.java.robert@gmail.com>
2016-05-01 18:58:11 -0400

    Script, para crear backups automáticamente

---
+++ automated_backups.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+format="musicdb"_`date +"%Y-%m-%d"`_`date +"%T"`
+pg_dump -i -h localhost -p 5432 -U admin_user -F c -b -v -f "/home/tobe/Documentos/Practica_1/backups/$format.backup" musicdb
+
+
